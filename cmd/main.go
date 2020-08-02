d8fd9e44c4918bc6a25282ab7a2018004acfb773
mayunmeiyouming <huangw@mvalley.io>
2020-08-02 17:38:17 +0800

    Feat：文件上传完成

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,25 @@
 package main
+
 import (
 	"log"
-	"github.com/mayunmeiyouming/FileStoreServer/internal/handler"
-	"github.com/mayunmeiyouming/FileStoreServer/internal/config"
 	"net/http"
+
+	"github.com/mayunmeiyouming/FileStoreServer/internal/config"
+	"github.com/mayunmeiyouming/FileStoreServer/internal/handler"
 )
+
+func handleListenPath(config config.Config) {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(config.RootPath+"/static"))))
+	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
+}
 
 func main() {
 	config := config.New()
-	http.HandleFunc("/file/upload", handler.UploadHandler)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(config.RootPath + "/static"))))
+	handleListenPath(config)
 	log.Println("设置监听")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Println("监听失败: " + err.Error())
 	}
-} 
+}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"os"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -12,7 +13,7 @@ func UploadHandler(response http.ResponseWriter, request *http.Request) {
 	log.Println("UploadHandler")
 	if request.Method == "GET" {
 		// 返回上传html页面
-		data, err := ioutil.ReadFile("./static/view/upload.html")
+		data, err := ioutil.ReadFile("./static/view/index.html")
 		if err != nil {
 			log.Println("文件读取失败")
 			io.WriteString(response, "internal server error ")
@@ -21,6 +22,39 @@ func UploadHandler(response http.ResponseWriter, request *http.Request) {
 		io.WriteString(response, string(data))
 	} else if request.Method == "POST" {
 		// 接受文件流及存储到本地目录
+		log.Println("文件上传中")
+		uploadFile, uploadFileHeader, err := request.FormFile("file")
+		if err != nil {
+			log.Println("文件上传出错")
+			response.WriteHeader(500)
+			io.WriteString(response, "internal server error ")
+			return
+		}
+		defer uploadFile.Close()
 
+		newFile, err := os.Create("/tmp/" + uploadFileHeader.Filename)
+		if err != nil {
+			log.Println("新文件创建失败")
+			response.WriteHeader(500)
+			io.WriteString(response, "internal server error ")
+			return
+		}
+		defer newFile.Close()
+		_, err = io.Copy(newFile, uploadFile)
+		if err != nil {
+			log.Println("文件保存失败")
+			response.WriteHeader(500)
+			io.WriteString(response, "internal server error ")
+			return
+		}
+		http.Redirect(response, request, "/file/upload/suc", http.StatusFound)
+		log.Println("文件上传成功")
+		return
 	}
+	log.Println("未知错误: ", request.Method)
+}
+
+// UploadSucHandler ...
+func UploadSucHandler(response http.ResponseWriter, request *http.Request) {
+	io.WriteString(response, "Upload Finished!")
 }
